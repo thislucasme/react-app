99a15450c292ec9c4e3bcc1718f7e594407a6055
Lucas Silva <lucassilvaee1245@gmail.com>
2022-04-08 17:06:50 -0300

    componente Login adcionado no App e button

--- src/login.tsx
+++ src/login.tsx
@@ -1,8 +1,8 @@
-export const login = () => {
+export const Login = () => {
     return (
         <>
-        <h1>Digite seu email aqui:</h1>
-        <h1>Digite sua senha aqui:</h1>
+            <h1>Digite seu email aqui:</h1>
+            <h1>Digite sua senha aqui:</h1>
         </>
     )
 }
